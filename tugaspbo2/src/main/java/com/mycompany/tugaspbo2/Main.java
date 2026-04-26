e3950842b46658108f0ca7ed3e9bb3e1383f6442
Shohibb11 <xunxan43@gmail.com>
2026-04-26 23:20:13 +0800

    Update Main.java

--- tugaspbo2/src/main/java/com/mycompany/tugaspbo2/Main.java
+++ tugaspbo2/src/main/java/com/mycompany/tugaspbo2/Main.java
@@ -3,7 +3,6 @@ package com.mycompany.tugaspbo2;
 public class Main {
     public static void main(String[] args) {
 
-        // Object dari constructor default
         Mahasiswa mhs1 = new Mahasiswa();
         System.out.println("=== Mahasiswa 1 (Default) ===");
         mhs1.tampilData();
@@ -11,7 +10,6 @@ public class Main {
 
         System.out.println();
 
-        // Object dari constructor parameter
         Mahasiswa mhs2 = new Mahasiswa("2410020054", "Shahibul", 3.7);
         System.out.println("=== Mahasiswa 2 ===");
         mhs2.tampilData();
